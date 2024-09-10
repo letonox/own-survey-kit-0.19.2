19aa032e01d3ecefb06dbd310b72ef0024fe1a1f
letonox <150264069+letonox@users.noreply.github.com>
2024-09-10 02:15:08 -0500

    FIX
    UPDATE

---
+++ lib/src/task/ordered_task.dart
@@ -0,0 +1,33 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:survey_kit/src/steps/step.dart';
+import 'package:survey_kit/src/task/task.dart';
+import 'package:survey_kit/src/task/identifier/task_identifier.dart';
+
+part 'ordered_task.g.dart';
+
+/// Defines a [Task] which handles its steps in the order of the [steps] list.
+@JsonSerializable(createFactory: false)
+class OrderedTask extends Task {
+  OrderedTask({
+    required TaskIdentifier id,
+    required List<Step> steps,
+    Step? initialStep,
+  }) : super(
+          id: id,
+          steps: steps,
+          initalStep: initialStep,
+        );
+
+  factory OrderedTask.fromJson(Map<String, dynamic> json) {
+    final steps = List<Map<String, dynamic>>.from(json['steps']);
+
+    return OrderedTask(
+      id: TaskIdentifier.fromJson(json),
+      steps: json['steps'] != null
+          ? steps.map((step) => Step.fromJson(step)).toList()
+          : [],
+    );
+  }
+
+  Map<String, dynamic> toJson() => _$OrderedTaskToJson(this);
+}
